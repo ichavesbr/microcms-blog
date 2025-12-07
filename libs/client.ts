ef319de94d89704178b4a876ea5e541b230a72f8
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-07 23:26:01 +0900

    change file's name for better organization

---
+++ libs/client.ts
@@ -0,0 +1,24 @@
+import { createClient } from "microcms-js-sdk"
+
+// lança erro se a variável de ambiente MICROCMS_SERVICE_DOMAIN não estiver definida
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required")
+}
+
+// lança erro se a variável de ambiente MICROCMS_API_KEY não estiver definida
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required")
+}
+
+// config variáveis de ambiente para receber dados do microcms
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
+  apiKey: process.env.MICROCMS_API_KEY || "",
+})
+
+
+// este arquivo configura o client para buscar dados do microcms
+// um nome ideial para este arquivo seria microcmsClient.ts ou similar tal quais as 3 op;:oes abaixo:
+// libs/microcmsClient.ts
+// libs/microcms-client.ts
+// libs/microcms/client.ts
