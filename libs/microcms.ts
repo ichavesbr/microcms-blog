7323e8df97fa72885476793e2c7a8410d419d076
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-07 17:33:49 +0900

    add error verification to lib file microcms.ts

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -1,6 +1,16 @@
 import { createClient } from "microcms-js-sdk"
 
-// config das variaveis de ambiente para receber dados do microcms
+// lança erro se a variável de ambiente MICROCMS_SERVICE_DOMAIN não estiver definida
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required")
+}
+
+// lança erro se a variável de ambiente MICROCMS_API_KEY não estiver definida
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required")
+}
+
+// config variáveis de ambiente para receber dados do microcms
 export const client = createClient({
   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
   apiKey: process.env.MICROCMS_API_KEY || "",
