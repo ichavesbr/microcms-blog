25f544b36a18dc2c085846b7e311ae83a50d5507
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-07 17:58:41 +0900

    sanitize html received from microCMS

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,18 @@ import { Props } from "@/types/microcms"
 
 export default async function Home() {
   const data = await client.get({ endpoint: "blogs" })
-  console.log("RESULTADO:", data)
 
   return (
     <div>
       <h1>Welcome</h1>
       <h2>To this blog</h2>
+      <br />
       <div>
         {data.contents.map((a: Props) => (
           <ul key={a.id}>
             <li>{a.title}</li>
-            <li>{JSON.stringify(a.content)}</li>
+            {/* previne ataques XSS  */}
+            <div dangerouslySetInnerHTML={{ __html: a.content }} />
           </ul>
         ))}
       </div>
