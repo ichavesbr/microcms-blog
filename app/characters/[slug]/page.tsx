e4d8b179a8574680f9fb902fc74eaf67289319c2
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-10 15:05:36 +0900

    refacture Character page - change .filter for .find do fetch api data

--- app/characters/[slug]/page.tsx
+++ app/characters/[slug]/page.tsx
@@ -1,23 +1,23 @@
 import fetchData from "@/libs/fetchData"
 import removeSpace from "@/libs/removeSpace"
 import Link from "next/link"
+import { Props } from "@/types/microcms"
 
 const Character = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params
   const data = await fetchData()
-  const currentCharacterData = data.contents.filter(item => removeSpace(item.name) === slug)
-  const { name, title, content } = currentCharacterData[0]
+  const { name, title, content } = data.contents.find((item: Props) => removeSpace(item.name) === slug)
 
   return (
     <div>
       <h1>{name} - Page</h1>
       <p>{title}</p>
-      <p dangerouslySetInnerHTML={{ __html: content }}></p>
+      {/* por algum motivo se encapsular com tag <p> da erro de hidratacao. Mas <div> funciona ok */}
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
 
       <button>
         <Link href="/characters">BACK</Link>
       </button>
-      {/* <button onClick={router.back}>BACK</button> */}
     </div>
   )
 }
