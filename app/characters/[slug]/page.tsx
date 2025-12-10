431936fcf95f096c4d9036370253d22a3caf2de4
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-10 14:45:28 +0900

    receives characters's data from microCMS

--- app/characters/[slug]/page.tsx
+++ app/characters/[slug]/page.tsx
@@ -1,13 +1,19 @@
+import fetchData from "@/libs/fetchData"
+import removeSpace from "@/libs/removeSpace"
 import Link from "next/link"
 
 const Character = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params
-
-  console.log(slug)
+  const data = await fetchData()
+  const currentCharacterData = data.contents.filter(item => removeSpace(item.name) === slug)
+  const { name, title, content } = currentCharacterData[0]
 
   return (
     <div>
-      <h1>{slug} - Page</h1>
+      <h1>{name} - Page</h1>
+      <p>{title}</p>
+      <p dangerouslySetInnerHTML={{ __html: content }}></p>
+
       <button>
         <Link href="/characters">BACK</Link>
       </button>
