efc8fe7ff3bac4bdab3dc572d026c86f02ef4723
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-13 11:45:07 +0900

    add minimun css to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,15 +8,15 @@ const Blog = async () => {
 
   return (
     <div>
-      <h1>PAGINA DO BLOG</h1>
+      <h1 className="text-center text-4xl my-8">SUNRIO CHARACTERS</h1>
       {/* <Link href="/blog/post1">Ir para meu primeiro post</Link> */}
 
       {data.contents.map((a: APIProps) => (
-        <ul key={a.id}>
-          <h1>{a.title}</h1>
-          <Link href={`/blog`}>Read More</Link>
+        <ul key={a.id} className="m-4 p-4 bg-amber-300 border-2">
+          <h1 className="text-4xl">{a.title}</h1>
+
           {/* previne ataques XSS  */}
-          <div dangerouslySetInnerHTML={{ __html: a.content }} />
+          <div dangerouslySetInnerHTML={{ __html: a.content }} className="py-4" />
 
           {/* verifica se existe eyecatch na api */}
           {a.eyecatch ? (
