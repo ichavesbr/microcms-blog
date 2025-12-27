7bcf5c7aefa788ca5573c863bc700ee03b00bbc0
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-27 22:19:05 +0900

    clean page.tsx from experimental test

--- app/COPILOT/page.tsx
+++ app/COPILOT/page.tsx
@@ -5,21 +5,11 @@ import { Input } from "./Input"
 export default async function Home() {
   const data = await fetchData()
 
-  // const myParam = searchParams?.param || ""
-  // const filtered = myParam ? data.filter((item: any) => item.title.toLowerCase().includes(myParam.toLowerCase())) : data
-
   return (
     <main className="min-h-screen flex items-center justify-center">
       <form>
         <Input /> {/* recebe dados e atualiza url */}
         <FilteredSearch data={data} />
-        <div>
-          {/* {filtered.map((item: any, index: number) => (
-            <div key={item.id}>
-              {index + 1} - {item.title} - {myParam}
-            </div>
-          ))} */}
-        </div>
       </form>
     </main>
   )
